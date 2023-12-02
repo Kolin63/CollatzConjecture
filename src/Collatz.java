157bbfa4380cefe134699d7a31f439ee11e20ec9
Kolin63 <theverylastgoodname@gmail.com>
2023-12-02 12:42:31 -0500

    v1.0.0

---
+++ src/Collatz.java
@@ -0,0 +1,48 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Collatz {
+
+    void run(){
+        // Initialization
+        int number;
+        int firstNumber;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\nEnter a Positive Integer: ");
+
+        do{
+            try {
+                number = scanner.nextInt();
+                firstNumber = number;
+            }catch (InputMismatchException e){
+                number = -1;
+                firstNumber = -1;
+                System.out.println("that aint a positive integer >:(");
+                scanner.next();
+            }
+        }while (number < 1);
+
+        // Starting the loop
+        int timesDone = 0;
+        System.out.println("\n" + timesDone + ". " + number);
+
+        do{
+            // If the number is even, divide by 2
+            if (number % 2 == 0){
+                number = number / 2;
+            }
+            // If the number is odd, multiply by 3 and add 1
+            else{
+                number = number * 3;
+                number++;
+            }
+            timesDone++;
+            System.out.println(timesDone + ". " + number);
+
+        }while (!(number == 1));
+
+        System.out.println("\n After " + timesDone + " calculations, " + firstNumber + " has been turned to 1.");
+
+        this.run();
+    }
+}
